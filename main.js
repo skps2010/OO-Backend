28827bef3e761236d712b76a393370e2047b7baa
skps2010 <skps2010030908@yahoo.com>
2020-05-06 19:42:23 +0800

    add config.js & getServerName

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var io = require('socket.io')({
     transports: ['websocket'],
 });
 var util = require('util')
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('config.json');
+var config = JSON.parse(rawdata);
 
 io.listen(4567);
 
@@ -9,4 +13,11 @@ io.on('connection', socket => {
     console.log('connected')
 
     socket.on('disconnect', () => { console.log('disconnected') })
+
+    socket.on('getServerName', () => {
+        console.log('send name');
+        socket.emit("getServerName", config["serverName"]);
+    })
 })
+
+console.log('開始')
